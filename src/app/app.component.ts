745e68277f03058592bcffe9252620afaa4c537f
Mohammad Uddin <s184174@student.dtu.dk>
2022-12-21 20:27:36 +0100

    login stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {KeycloakProfile} from "keycloak-js";
+import {KeycloakService} from "keycloak-angular";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'poc_application';
+  public isLoggedin = false;
+  public profile: KeycloakProfile | null = null;
+
+  constructor(private readonly keycloak: KeycloakService) {
+  }
+
+  public async ngOnInit(){
+    // this.loginSession();
+    this.isLoggedin = await this.keycloak.isLoggedIn();
+
+    type roleUsers = Array<{id: number, text: string}>;
+
+    if (this.isLoggedin) {
+      this.profile = await this.keycloak.loadUserProfile();
+      console.log(this.keycloak.getToken());
+      //console.log(this.keycloak.register());
+    } else {
+      this.loginSession();
+    }
+  }
+
+  public loginSession() {
+    this.keycloak.login();
+  }
+
+  public logoutSession(){
+    this.keycloak.logout();
+  }
 }
